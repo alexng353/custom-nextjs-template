7c837eae62ef78246d06d830cafa007e9b730fa5
Alexander Ng <alex@ayo.icu>
2022-06-22 20:48:07 -0700

    added readme.py

---
+++ README.md
@@ -0,0 +1,16 @@
+Download main.py from github and put it into the main folder you code out of
+
+cd into that folder and run 
+```bash
+python main.py --ts --tailwind --mui
+```
+
+Enter the name of your nextjs project when it prompts you and go on your merry way
+
+
+#
+
+If you want to decrease the number of steps, run
+```bash
+python main.py --name <<name>> --ts --tailwind --mui
+```
---
+++ main.py
@@ -0,0 +1,78 @@
+import os, sys
+
+# read cli args and set variables
+args = sys.argv[1:]
+if len(args) == 0:
+    print("No arguments given")
+    sys.exit(1)
+if "--ts" in args:
+    ts = True
+else:
+    ts = False
+if "--name" in args:
+    # find the index of the --name argument
+    name = args[args.index("--name")+1]
+else:
+    name = input("Name:\n>>>")
+
+tailwind = True if "--tailwind" in args else False
+mui = True if "--mui" in args else False
+
+tailwind_config_js = """/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}"""
+
+globals_css = """@tailwind base;
+@tailwind components;
+@tailwind utilities;"""
+
+index_page = """import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next MUI TailwindCSS App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <a href="https://nextjs.org" style={{color:"blue", textDecoration:"underline"}} target="blank">Learn Next.js</a>
+        <br/>
+        <a href="https://mui.com" style={{color:"blue", textDecoration:"underline"}}>Learn MaterialUI</a>
+        <br/>
+        <a href="https://tailwindcss.com" style={{color:"blue", textDecoration:"underline", }}>Learn TailwindCSS</a>
+      </div>
+    </div>
+  )
+}
+
+export default Home"""
+
+os.system(f'npx create-next-app@latest {name} {"--ts" if ts else ""}')
+os.system(f'cd {name}')
+if tailwind:
+    os.system(f'cd {name} && npm install -D tailwindcss postcss autoprefixer')
+    os.system(f'cd {name} && npx tailwindcss init -p')
+    f = open(f"{name}/tailwind.config.js", "w")
+    f.write(tailwind_config_js)
+    f.close()
+    g = open(f"{name}/styles/globals.css", "w")
+    g.write(globals_css)
+    g.close()
+    os.system(f'rm {name}/styles/Home.module.css')
+    os.system(f'rm {name}/pages/index.{"tsx" if ts else "js"}')
+    h = open(f"{name}/pages/index.{'tsx' if ts else 'js'}", "w")
+    h.write(index_page)
+    h.close()
+    
+if mui:
+    os.system(f'cd {name} && npm install @mui/material @emotion/react @emotion/styled')
